cd78a91e4889cac9deff7e6d0cfaa3af7d8485a6
Gabmeister <gabriel@gabrielhorvat.com>
2020-10-19 16:28:46 +0200

    refactor checkout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,50 +1,46 @@
-import React from "react";
-import "./checkout.styles.scss";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import { selectCartItems } from "../../redux/cart/cart.selectors";
-import { selectCartTotal } from "../../redux/cart/cart.selectors";
-import { withRouter } from "react-router-dom";
+import React, { useContext } from "react";
+
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
-        <span>Product</span>
-      </div>
-      <div className="header-block">
-        <span>Description</span>
-      </div>
-      <div className="header-block">
-        <span>Quantity</span>
-      </div>
-      <div className="header-block">
-        <span>Price</span>
-      </div>
-      <div className="header-block">
-        <span>Remove</span>
-      </div>
-    </div>
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    <div className="total">
-      <span> Total: ${total}</span>
-    </div>
-    <div className="test-warning">
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - Exp: 03/22 - CVV: 123
-    </div>
-    <StripeCheckoutButton price={total} />
-  </div>
-);
+import { CartContext } from "../../providers/cart/cart.provider";
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal,
-});
+import "./checkout.styles.scss";
 
-export default withRouter(connect(mapStateToProps)(CheckoutPage));
+const CheckoutPage = () => {
+  const { cartItems, cartTotal } = useContext(CartContext);
+
+  return (
+    <div className="checkout-page">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+        <div className="header-block">
+          <span>Quantity</span>
+        </div>
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">TOTAL: ${cartTotal}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={cartTotal} />
+    </div>
+  );
+};
+
+export default CheckoutPage;
